0c834d3b4c0aacb6f74e1ab0f93e6ae967235ef0
Mahendra <mksuthar9016@gmail.com>
2023-09-15 00:37:29 +0530

    changes

--- config/redis.py
+++ config/redis.py
@@ -4,6 +4,6 @@ from .config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
 redis_client = redis.StrictRedis(
     host=REDIS_HOST,
     port=REDIS_PORT,
-    # password=REDIS_PASSWORD,
+    password=REDIS_PASSWORD,
     db=REDIS_DB
 )
